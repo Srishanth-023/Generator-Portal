ba4beeda84e25b4b48d8c7413d28231d1070d484
Boomathi-7 <boomathi2047@gmail.com>
2025-12-05 09:55:23 +0530

    Front Page added

---
+++ Front-page/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Add your other routes here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
